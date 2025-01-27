d2e10581a443e19a17898b9c09e791dcd86fa139
nikiiigoyal <nikiiigoyal@gmail.com>
2025-01-27 10:15:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,347 @@
+import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+import { fileURLToPath } from "url";
+import pkg from "pg";
+const { Pool } = pkg;
+
+// Create a new connection pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+    },
+  },
+});
+
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.register(fastifyStatic, {
+  root: path.join(__dirname, "public"),
+  prefix: "/public/",
+});
+
+// Helper function to execute queries
+async function query(text, params = []) {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(text, params);
+    return result.rows;
+  } finally {
+    client.release();
+  }
+}
+
+server.get("/api/pizzas", async function getPizzas(req, res) {
+  const pizzasPromise = query(`
+    SELECT pizza_type_id, name, category, ingredients as description 
+    FROM pizza_types
+  `);
+
+  const pizzaSizesPromise = query(`
+    SELECT 
+      pizza_type_id as id, size, price
+    FROM 
+      pizzas
+  `);
+
+  const [pizzas, pizzaSizes] = await Promise.all([
+    pizzasPromise,
+    pizzaSizesPromise,
+  ]);
+
+  const responsePizzas = pizzas.map((pizza) => {
+    const sizes = pizzaSizes.reduce((acc, current) => {
+      if (current.id === pizza.pizza_type_id) {
+        acc[current.size] = +current.price;
+      }
+      return acc;
+    }, {});
+    return {
+      id: pizza.pizza_type_id,
+      name: pizza.name,
+      category: pizza.category,
+      description: pizza.description,
+      image: `/public/pizzas/${pizza.pizza_type_id}.webp`,
+      sizes,
+    };
+  });
+
+  res.send(responsePizzas);
+});
+
+server.get("/api/pizza-of-the-day", async function getPizzaOfTheDay(req, res) {
+  const pizzas = await query(`
+    SELECT 
+      pizza_type_id as id, name, category, ingredients as description
+    FROM 
+      pizza_types
+  `);
+
+  const daysSinceEpoch = Math.floor(Date.now() / 86400000);
+  const pizzaIndex = daysSinceEpoch % pizzas.length;
+  const pizza = pizzas[pizzaIndex];
+
+  const sizes = await query(
+    `
+    SELECT
+      size, price
+    FROM
+      pizzas
+    WHERE
+      pizza_type_id = $1
+  `,
+    [pizza.id]
+  );
+
+  const sizeObj = sizes.reduce((acc, current) => {
+    acc[current.size] = +current.price;
+    return acc;
+  }, {});
+
+  const responsePizza = {
+    id: pizza.id,
+    name: pizza.name,
+    category: pizza.category,
+    description: pizza.description,
+    image: `/public/pizzas/${pizza.id}.webp`,
+    sizes: sizeObj,
+  };
+
+  res.send(responsePizza);
+});
+
+server.get("/api/orders", async function getOrders(req, res) {
+  const orders = await query("SELECT order_id, date, time FROM orders");
+  res.send(orders);
+});
+
+server.get("/api/order", async function getOrders(req, res) {
+  const id = req.query.id;
+  const orderPromise = query(
+    "SELECT order_id, date, time FROM orders WHERE order_id = $1",
+    [id]
+  );
+
+  const orderItemsPromise = query(
+    `
+    SELECT 
+      t.pizza_type_id as pizzaTypeId, t.name, t.category, t.ingredients as description, 
+      o.quantity, p.price, o.quantity * p.price as total, p.size
+    FROM 
+      order_details o
+    JOIN
+      pizzas p
+    ON
+      o.pizza_id = p.pizza_id
+    JOIN
+      pizza_types t
+    ON
+      p.pizza_type_id = t.pizza_type_id
+    WHERE 
+      order_id = $1
+  `,
+    [id]
+  );
+
+  const [order, orderItemsRes] = await Promise.all([
+    orderPromise,
+    orderItemsPromise,
+  ]);
+
+  const orderItems = orderItemsRes.map((item) =>
+    Object.assign({}, item, {
+      image: `/public/pizzas/${item.pizzaTypeId}.webp`,
+      quantity: +item.quantity,
+      price: +item.price,
+    })
+  );
+
+  const total = orderItems.reduce((acc, item) => acc + item.total, 0);
+
+  res.send({
+    order: Object.assign({ total }, order[0]),
+    orderItems,
+  });
+});
+
+server.post("/api/order", async function createOrder(req, res) {
+  const { cart } = req.body;
+
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", { hour12: false });
+  const date = now.toISOString().split("T")[0];
+
+  if (!cart || !Array.isArray(cart) || cart.length === 0) {
+    res.status(400).send({ error: "Invalid order data" });
+    return;
+  }
+
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+
+    const result = await client.query(
+      "INSERT INTO orders (date, time) VALUES ($1, $2) RETURNING order_id",
+      [date, time]
+    );
+    const orderId = result.rows[0].order_id;
+
+    const mergedCart = cart.reduce((acc, item) => {
+      const id = item.pizza.id;
+      const size = item.size.toLowerCase();
+      if (!id || !size) {
+        throw new Error("Invalid item data");
+      }
+      const pizzaId = `${id}_${size}`;
+
+      if (!acc[pizzaId]) {
+        acc[pizzaId] = { pizzaId, quantity: 1 };
+      } else {
+        acc[pizzaId].quantity += 1;
+      }
+
+      return acc;
+    }, {});
+
+    for (const item of Object.values(mergedCart)) {
+      const { pizzaId, quantity } = item;
+      await client.query(
+        "INSERT INTO order_details (order_id, pizza_id, quantity) VALUES ($1, $2, $3)",
+        [orderId, pizzaId, quantity]
+      );
+    }
+
+    await client.query("COMMIT");
+    res.send({ orderId });
+  } catch (error) {
+    await client.query("ROLLBACK");
+    req.log.error(error);
+    res.status(500).send({ error: "Failed to create order" });
+  } finally {
+    client.release();
+  }
+});
+
+server.get("/api/past-orders", async function getPastOrders(req, res) {
+  try {
+    const page = Number.parseInt(req.query.page, 10) || 1;
+    const limit = 20;
+    const offset = (page - 1) * limit;
+    const pastOrders = await query(
+      `
+      SELECT order_id, date, time 
+      FROM orders 
+      ORDER BY order_id DESC 
+      LIMIT $1 
+      OFFSET $2
+    `,
+      [limit, offset]
+    );
+    res.send(pastOrders);
+  } catch (error) {
+    req.log.error(error);
+    res.status(500).send({ error: "Failed to fetch past orders" });
+  }
+});
+
+server.get("/api/past-order/:order_id", async function getPastOrder(req, res) {
+  const orderId = req.params.order_id;
+
+  try {
+    const order = await query(
+      `
+      SELECT order_id, date, time 
+      FROM orders 
+      WHERE order_id = $1
+    `,
+      [orderId]
+    );
+
+    if (order.length === 0) {
+      res.status(404).send({ error: "Order not found" });
+      return;
+    }
+
+    const orderItems = await query(
+      `
+      SELECT 
+        t.pizza_type_id as pizzaTypeId, t.name, t.category, t.ingredients as description, 
+        o.quantity, p.price, o.quantity * p.price as total, p.size
+      FROM 
+        order_details o
+      JOIN
+        pizzas p
+      ON
+        o.pizza_id = p.pizza_id
+      JOIN
+        pizza_types t
+      ON
+        p.pizza_type_id = t.pizza_type_id
+      WHERE 
+        order_id = $1
+    `,
+      [orderId]
+    );
+
+    const formattedOrderItems = orderItems.map((item) =>
+      Object.assign({}, item, {
+        image: `/public/pizzas/${item.pizzaTypeId}.webp`,
+        quantity: +item.quantity,
+        price: +item.price,
+      })
+    );
+
+    const total = formattedOrderItems.reduce(
+      (acc, item) => acc + item.total,
+      0
+    );
+
+    res.send({
+      order: Object.assign({ total }, order[0]),
+      orderItems: formattedOrderItems,
+    });
+  } catch (error) {
+    req.log.error(error);
+    res.status(500).send({ error: "Failed to fetch order" });
+  }
+});
+
+server.post("/api/contact", async function contactForm(req, res) {
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    res.status(400).send({ error: "All fields are required" });
+    return;
+  }
+
+  req.log.info(`Contact Form Submission:
+    Name: ${name}
+    Email: ${email}
+    Message: ${message}
+  `);
+
+  res.send({ success: "Message received" });
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: PORT });
+    console.log(`Server listening on port ${PORT}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+start();
